da67e1de12bff98860426fa765ef667561fa96bb
olumide-AI <olumidekolawole26@gmail.com>
2025-05-24 01:20:33 -0700

    Topping test for both regular and premium toppings

---
+++ src/test/java/com/delicioussandwich/model/ToppingTest.java
@@ -0,0 +1,37 @@
+package com.delicioussandwich.model;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ToppingTest {
+    @Test
+    public void testRegularToppingIsZero(){
+        //Arrange - Create new topping
+        Topping topping = new RegularTopping("Spinach");
+        //Act and Arrange
+        assertEquals(0.0, topping.getPrice("4"), 0.001);
+        assertEquals(0.0, topping.getPrice("8"), 0.001);
+        assertEquals(0.0, topping.getPrice("12"), 0.001);
+
+    }
+    @Test
+    public void testPremiumToppingWithNormalPrice(){
+        //Arrange - Create new Topping
+        Topping topping = new PremiumTopping("Turkey", false);
+        //Act and Arrange
+        assertEquals(1.0, topping.getPrice("4"), 0.001);
+        assertEquals(2.0, topping.getPrice("8") , 0.001);
+        assertEquals(3.0, topping.getPrice("12") , 0.001);
+    }
+
+    @Test
+    public void testPremiumToppingWithExtraTopping(){
+        //Arrange - Create new Topping
+        Topping topping = new PremiumTopping("Turkey", true);
+        //Act and Arrange
+        assertEquals(1.50, topping.getPrice("4"), 0.001);
+        assertEquals(3.0, topping.getPrice("8"), 0.001);
+        assertEquals(4.50, topping.getPrice("12"), 0.001);
+    }
+}
