821eff05480d47ebf34653f9b15e0b4d086efcf2
olumide-AI <olumidekolawole26@gmail.com>
2025-05-24 03:38:25 -0700

    customer feature

---
+++ src/main/java/com/delicioussandwich/model/Customer.java
@@ -0,0 +1,34 @@
+package com.delicioussandwich.model;
+
+public class Customer {
+    //Properties
+    private String name;
+    private String phoneNumber;
+
+    //Constructor
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //Getters and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString(){
+        return name + " (" + phoneNumber + ")";
+    }
+}
