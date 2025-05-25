e3ce762fcf3f3fda5ff364b5e01da52ee7451239
olumide-AI <olumidekolawole26@gmail.com>
2025-05-25 03:19:12 -0700

    refactored premium toppings

--- src/main/java/com/delicioussandwich/model/PremiumTopping.java
+++ src/main/java/com/delicioussandwich/model/PremiumTopping.java
@@ -7,15 +7,25 @@ package com.delicioussandwich.model;
 
 public class PremiumTopping extends Topping{
     private boolean extraTopping;
+    private String toppingCategory;
 
     //Constructor
-    public PremiumTopping(String name, boolean extraTopping) {
+    public PremiumTopping(String name,  String toppingCategory, boolean extraTopping) {
         super(name);
         this.extraTopping = extraTopping;
+        this.toppingCategory = toppingCategory;
     }
 
     //Getters and Setters
 
+
+    public String getToppingCategory() {
+        return toppingCategory;
+    }
+
+    public void setToppingCategory(String toppingCategory) {
+        this.toppingCategory = toppingCategory;
+    }
 
     public boolean isExtraTopping() {
         return extraTopping;
@@ -27,31 +37,56 @@ public class PremiumTopping extends Topping{
 
     @Override
     public double getPrice(String sandwichSize) {
-        if(sandwichSize.equalsIgnoreCase("4")){
-            if(extraTopping == true){
-                return 1.50;
+        sandwichSize = sandwichSize.trim();
+        if (toppingCategory.equalsIgnoreCase("meat")){
+            if(sandwichSize.equalsIgnoreCase("4")){
+                if(extraTopping == true){
+                    return 1.50;
+                }
+                else {
+                    return 1.00;
+                }
+            } else if (sandwichSize.equalsIgnoreCase("8")) {
+                if(extraTopping == true){
+                    return 3.00;
+                }
+                else {
+                    return 2.00;
+                }
+            } else if (sandwichSize.equalsIgnoreCase('12')) {
+                if (extraTopping == true){
+                    return 4.50;
+                }
+                else {
+                    return 3.00;
+                }
             }
-            else{
-                return 1.00;
-            }
-        } else if (sandwichSize.equalsIgnoreCase("8")) {
-            if(extraTopping == true){
-                return 3.00;
-            }
-            else{
-                return 2.00;
-            }
-        } else if (sandwichSize.equalsIgnoreCase("12")) {
-            if(extraTopping == true){
-                return 4.50;
-            }
-            else{
-                return 3.00;
+        } else if (toppingCategory.equalsIgnoreCase("cheese")) {
+            if(sandwichSize.equalsIgnoreCase("4")){
+                if(extraTopping == true){
+                    return 1.05;
+                }
+                else {
+                    return 0.75;
+                }
+            } else if (sandwichSize.equalsIgnoreCase("8")) {
+                if(extraTopping == true){
+                    return 2.10;
+                }
+                else {
+                    return 1.50;
+                }
+            } else if (sandwichSize.equalsIgnoreCase("12")) {
+                if(extraTopping == true){
+                    return 3.15;
+                }
+                else {
+                    return 2.25;
+                }
             }
         }
-        else{
-            return 0.0;
-        }
+        System.out.println("Not a category");
+        return 0.0;
     }
     @Override
     public String toString(){
@@ -61,7 +96,7 @@ public class PremiumTopping extends Topping{
     @Override
     public String getName(){
         if (extraTopping == true){
-            return "Extra Premium" + name;
+            return "Extra Premium " + name;
         }
         else {
             return name;
