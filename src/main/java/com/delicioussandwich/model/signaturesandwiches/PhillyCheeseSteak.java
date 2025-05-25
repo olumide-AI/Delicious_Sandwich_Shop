f377c16013670cc1955962b7c4b49160c159d873
olumide-AI <olumidekolawole26@gmail.com>
2025-05-25 06:50:36 -0700

    Philly cheese steak

---
+++ src/main/java/com/delicioussandwich/model/signaturesandwiches/PhillyCheeseSteak.java
@@ -0,0 +1,16 @@
+package com.delicioussandwich.model.signaturesandwiches;
+
+import com.delicioussandwich.model.abstractclass.SignatureSandwich;
+import com.delicioussandwich.model.topping.PremiumTopping;
+import com.delicioussandwich.model.topping.RegularTopping;
+
+public class PhillyCheeseSteak extends SignatureSandwich {
+    public PhillyCheeseSteak(String breadType) {
+        super("Philly Cheese Steak", breadType);
+        addTopping(new PremiumTopping("Bacon", "meat", false));
+        addTopping(new PremiumTopping("Cheddar", "chese", false));
+        addTopping(new RegularTopping("Lettuce"));
+        addTopping(new RegularTopping("Tomato"));
+        addTopping(new RegularTopping("Ranch"));
+    }
+}
