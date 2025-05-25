8706f3d1ff009a6e237ab9c9d417ee2b14425d91
olumide-AI <olumidekolawole26@gmail.com>
2025-05-25 04:44:48 -0700

    user input for drinking

--- src/main/java/com/delicioussandwich/app/Application.java
+++ src/main/java/com/delicioussandwich/app/Application.java
@@ -1,7 +1,6 @@
 package com.delicioussandwich.app;
 
-import com.delicioussandwich.model.Order;
-import com.delicioussandwich.model.Sandwich;
+import com.delicioussandwich.model.*;
 
 import java.util.Scanner;
 
@@ -15,11 +14,16 @@ public class Application {
             String userInput = scanner.nextLine().toLowerCase().trim();
 
             switch (userInput){
-                case '1':
+                case "1":
                     newOrder();
                     break;
+                case "0":
+                    System.out.println("Goodbye and thank you ");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please Select (1 or 0): ");
             }
-
         }
     }
     private static void homeScreen(){
@@ -35,9 +39,16 @@ public class Application {
         while(isOrdering){
             orderScreen();
             String userInput = scanner.nextLine().toLowerCase().trim();
+
             switch (userInput){
                 case "1":
-                    Sandwich sandwich =
+                    Sandwich sandwich = buildUserSandwich();
+                    order.addItem(sandwich);
+                    System.out.println("Sandwich has been added to order");
+                    break;
+                case "2":
+                    Drink drink
+
             }
         }
     }
@@ -49,5 +60,62 @@ public class Application {
         System.out.println("Select '3' to Add a chip");
         System.out.println("Select '4' to checkout sandwich");
         System.out.println("Select '0' to cancel order");
+    }
+
+    private static Sandwich buildUserSandwich(){
+        System.out.println("Let's make your sandwich according to what you like");
+        System.out.println("========");
+
+        System.out.println("Enter bread type (white, wheat, wrap or rye): ");
+        String breadType = scanner.nextLine().toLowerCase().trim();
+
+        System.out.println("Enter sandwich side (4, 8, or 12): ");
+        String sandwichSize = scanner.nextLine().toLowerCase().trim();
+
+        System.out.println("Do you want sandwich toasted (yes/no): ");
+        boolean sandwichToasted = scanner.nextLine().toLowerCase().trim().equalsIgnoreCase("yes");
+
+        Sandwich sandwich = new Sandwich(sandwichSize, breadType, sandwichToasted);
+
+        //Ask user for topping options
+        while (true){
+            System.out.println("Ass a topping or type 'done' when finished: ");
+            String toppingName = scanner.nextLine().toLowerCase().trim();
+            if(toppingName.equalsIgnoreCase("done")){
+                break;
+            }
+
+            System.out.println("Would you like premium or regular toppings today?");
+            String toppingType = scanner.nextLine().toLowerCase().trim();
+            if(toppingType.equalsIgnoreCase("regular")){
+                sandwich.addTopping(new RegularTopping(toppingName));
+            }
+            else{
+                System.out.println("Would you like meat or cheese? ");
+                String premiumToppingType = scanner.nextLine().toLowerCase().trim();
+                System.out.println("Would you like extra topping today? (yes or no): ");
+                boolean extraTopping = scanner.nextLine().equalsIgnoreCase("yes");
+                sandwich.addTopping(new PremiumTopping(toppingName, premiumToppingType, extraTopping));
+            }
+        }
+        return sandwich;
+    }
+
+    private static Drink buildUserDrink(){
+        System.out.println("Enter drink of choice: ");
+        String userInput = scanner.nextLine().toLowerCase().trim();
+        String drinkCupSize = "";
+
+        while(true){
+            System.out.println("Choose cup size (small, medium or large): ");
+            drinkCupSize = scanner.nextLine().toLowerCase().trim();
+            if((drinkCupSize.equalsIgnoreCase("small")) ||drinkCupSize.equalsIgnoreCase("medium") || drinkCupSize.equalsIgnoreCase("large")){
+                break;
+            }
+            else {
+                System.out.println("Please select an appropriate cup size");
+            }
+        }
+        return new Drink(userInput, drinkCupSize);
     }
 }
