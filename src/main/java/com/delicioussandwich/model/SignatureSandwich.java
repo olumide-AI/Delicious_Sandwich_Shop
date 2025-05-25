c4d779ae80da9079a24c51ef2764cf7a68d1574d
olumide-AI <olumidekolawole26@gmail.com>
2025-05-25 06:34:11 -0700

    signaturesandwich

---
+++ src/main/java/com/delicioussandwich/model/SignatureSandwich.java
@@ -0,0 +1,11 @@
+package com.delicioussandwich.model;
+
+public abstract class SignatureSandwich extends Sandwich{
+
+    public SignatureSandwich(String sigSandwichName, String breadType) {
+        super("8", breadType, true);
+        super.setName(sigSandwichName);
+    }
+
+    public void se
+}
