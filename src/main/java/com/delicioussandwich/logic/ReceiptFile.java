24c3a03a2a4eb8eb1fa89cc0673c15f5dbe16bda
olumide-AI <olumidekolawole26@gmail.com>
2025-05-25 02:47:27 -0700

    save ro recipt file feature implemented

---
+++ src/main/java/com/delicioussandwich/logic/ReceiptFile.java
@@ -0,0 +1,32 @@
+package com.delicioussandwich.logic;
+
+import com.delicioussandwich.model.Order;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReceiptFile {
+    private static final String RECEIPT_FOLDER = "receipts";
+
+    public static void saveReceiptToFile(Order order){
+        try{
+            Files.createDirectories(Path.of(RECEIPT_FOLDER));
+            String receiptDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+            String receiptFileName = RECEIPT_FOLDER + "/" + receiptDateTime + ".txt";
+
+            String receiptInfo = Receipt.generateReceipt(order);
+            FileWriter fileWriter = new FileWriter(receiptFileName);
+            fileWriter.write(receiptInfo);
+            fileWriter.close();
+
+            System.out.println("Receipt saved to: " + receiptFileName);
+        }
+        catch (IOException e){
+            System.out.println("File saving error");
+        }
+    }
+}
